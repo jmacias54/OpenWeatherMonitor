cb018cafa5a98dfab63fd14597a0bf181691f0ce
jesus armando macias benitez <jesusmb@finerioconnect.com>
2023-12-22 13:16:37 -0600

    first commit

---
+++ src/main/java/com/mx/openweathermap/monitor/OpenWeatherMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.mx.openweathermap.monitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenWeatherMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenWeatherMonitorApplication.class, args);
+	}
+
+}
